c8be000f7e66730750df0998b2349be6b290e978
Tiago Matos Guedes <79611898+guedes674@users.noreply.github.com>
2022-03-22 14:19:45 +0000

    Update miniteste2.c

--- minitestes/miniteste2.c
+++ minitestes/miniteste2.c
@@ -4,12 +4,12 @@ void fibonacci () {
   int a = 0, b = 1, aux, n = 50, r;
   for (; n > 0; n--) {
     printf("%d\n",a);
-    aux = a;
-    a = b;
-    b = (b + aux) % 10000;
     if (a % 2 == 0){
         r += 1;
     }
+    aux = a;
+    a = b;
+    b = (b + aux) % 10000;
   }
   printf("%d", r);
 }
