53a4a9ec8406dcf564210c3f027858831f3b3ad8
guedes674 <guedes674@guedes>
2024-02-20 17:51:03 +0000

    a realizar ficha 2

--- Fichas/ficha2.c
+++ Fichas/ficha2.c
@@ -6,8 +6,40 @@ float multInt1 (int n, float m){
     float r = 0;
     int i;
 
-    for (i = 0; r <= (n*m); i++)
-        r += (i*m);
+    for (i = 0; r < (n*m); i++)
+        r += m;
 
     return r;
+}
+
+// Exercício 2
+
+float multInt2 (int n, float m){
+    float r = 0;
+    int i;
+
+    for(i = 1; n >= 1; i++){
+        if (n % 2 != 0){
+            r += m;
+        }
+        n /= 2;
+        m *= 2;
+    }
+    return r;
+}
+
+// Exercício 3
+
+int mdc1 (int a, int b){
+    int i , menor = a;
+    
+    if (a > b)
+        menor = b;
+    
+    i = menor;
+
+    while ((a%i == 0 && b%i == 0) == 0){
+        i--;
+    }
+    return i;
 }
