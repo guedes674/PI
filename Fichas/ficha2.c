dd14e908b08c6bff14ba8bd9c20112d5df2c1b8d
guedes674 <guedes674@guedes>
2024-02-21 17:45:07 +0000

    a continuar a ficha2

--- Fichas/ficha2.c
+++ Fichas/ficha2.c
@@ -28,6 +28,22 @@ float multInt2 (int n, float m){
     return r;
 }
 
+float multInt3 (int n, float m, int *count){
+    float r = 0;
+    int i;
+    *count = 0;
+
+    for(i = 1; n >= 1; i++){
+        (*count)++;
+        if (n % 2 != 0){
+            r += m;
+        }
+        n /= 2;
+        m *= 2;
+    }
+    return r;
+}
+
 // Exercício 3
 
 int mdc1 (int a, int b){
@@ -42,4 +58,62 @@ int mdc1 (int a, int b){
         i--;
     }
     return i;
+}
+
+// Exercício 4
+
+int mdc2 (int a, int b){
+    while (a != b){
+        if (a > b)
+            a -= b;
+        else if (a < b)
+            b -= a;
+    }
+    return a;
+}
+
+int mdc3 (int a, int b, int *count){
+    *count = 0;
+    while (a != b){
+        (*count)++;
+        if (a > b)
+            a -= b;
+        else if (a < b)
+            b -= a;
+    }
+    return a;
+}
+
+// Exercício 5
+
+int mdc4 (int a, int b, int *count){
+    int res = 1;
+    *count = 0;
+    while (a > 0 && b > 0){
+        (*count)++;
+        if (a > b)
+            a %= b;
+        else if (a < b)
+            b %= a;
+    }
+    if (a == 0)
+        res = b;
+    else
+        res = a;
+    return res;
+}
+
+// Exercício 6
+
+// A
+
+int fib1 (int n){
+    int res = 0;
+
+    while(n >= 2){
+        res = fib1(n-1) + fib1(n-2);
+        n --;
+    }
+    res += 1;
+    return res;
 }
