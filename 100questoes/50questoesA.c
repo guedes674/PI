7d9b16ac5f23a20f2535ec0ef71f3b8e50d414f8
guedes674 <guedes674@guedes>
2024-03-02 22:13:30 +0000

    a realizar as 50 questoes

--- 100questoes/50questoesA.c
+++ 100questoes/50questoesA.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
 
 //1
 int maior(){
@@ -183,6 +184,100 @@ void strnoV (char s[]){
     }
     newstr[j] = '\0';
     strcpy(s,newstr);
+}
+
+//13
+void truncW (char t[], int n){
+    int i = 0, j = 0, aux = n;
+
+    while(t[i] != '\0'){
+        if(isspace(t[i])){
+            t[j] = t[i];
+            j++;
+            i++;
+            aux = n;
+        }else if((isalpha(t[i]) || ispunct(t[i]) || iscntrl(t[i])) && aux > 0){
+            t[j] = t[i];
+            j++;
+            i++;
+            aux--;
+        }else{
+            i++;
+        }
+    }
+    t[j] = '\0';
+}
+
+//14
+char charMaisfreq (char s[]){
+    int i = 0, j = 0, timesC = 0, acc = 0;
+    char c = s[i];
+    if(s[i] != '\0'){
+        for(i = 0; s[i] != '\0'; i++){
+            for(j = 0; s[j] != '\0'; j++){
+                if(s[j] == s[i]){
+                    acc++;
+                }
+            }
+            if(acc > timesC){
+                timesC = acc;
+                c = s[i];
+            }
+            acc = 0;
+        }
+    }else{
+        c = '0';
+    }
+    return c;
+}
+
+//15
+int iguaisConsecutivos (char s[]){
+    int i = 0, acc = 1, maiorSeq = 0;
+
+    while(s[i] != '\0'){
+        if (s[i] == s[i+1]){
+            acc++;
+        }else{
+            if(acc > maiorSeq){
+                maiorSeq = acc;
+            }
+            acc = 1;
+        }
+        i++;
+    }
+    return maiorSeq;
+}
+
+//16
+int na_anterior (char s[], int a, int b){
+    int i, ret = 1;
+
+    for(i = a; i < b; i++){
+        if(s[i] == s[b]){
+            return 0;
+        }
+    }
+    return ret;
+}
+
+int difConsecutivos (char s[]){
+    int i = 0, j = 0, acc = 0, maiorSeq = 0;
+
+    for(i = 0; s[i]; i++){
+        for(j = i; s[j]; j++){
+            if(na_anterior(s,i,j)){
+                acc++;
+            }else{
+                break;
+            }
+        }
+        if(acc > maiorSeq){
+            maiorSeq = acc;
+        }
+        acc = 0;
+    }
+    return maiorSeq;
 }
 
 int main(){
