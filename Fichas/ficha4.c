e5f067a53ce0d8a2b44373b673f6c783ea689900
guedes674 <guedes674@guedes>
2024-03-12 19:27:41 +0000

    ficha 4 terminada

--- Fichas/ficha4.c
+++ Fichas/ficha4.c
@@ -39,3 +39,84 @@ int retiraVogaisRep (char *s){
     }
     return count;
 }
+
+//3
+int duplicaVogais (char *s){
+    int count = 0, i = 0, j = 0;
+
+    while(s[i] != '\0'){
+        if(eVogal(s[i])){
+            j = strlen(s);
+            while(j > i){
+                s[j+1] = s[j];
+                j--;
+            }
+            s[i+1] = s[i];
+            i++;
+            j = 0;
+            count++;
+        }
+        i++;
+    }
+    return count;
+}
+
+//4
+int ordenado (int a[], int N){
+    int i = 0;
+
+    while(i < N){
+        if(a[i] > a[i+1]){
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+//5
+void merge (int a[], int na, int b[], int nb, int r[]){
+    int i = 0, j = 0, k = 0;
+
+    while(i < na && j < nb){
+        if(a[i] <= b[j]){
+            r[k] = a[i];
+            i++;
+        }else{
+            r[k] = b[j];
+            j++;
+        }
+        k++;
+    }
+    if (i == na){
+        while(j < nb){
+            r[k] = b[j];
+            j++;
+            k++;
+        }
+    }else{
+        while(i < na){
+            r[k] = a[i];
+            i++;
+            k++;
+        }
+    }
+}
+
+//3
+int partition (int v[], int N, int x){
+    int i = 0, j = N-1, count = 0, aux = -1;
+
+    while(i < j+1){
+        if(v[i] <= x){
+            i++;
+            count++;
+        }else{
+            aux = v[i];
+            v[i] = v[j];
+            v[j] = aux;
+            j--;
+        }
+    }
+    return count;
+}
