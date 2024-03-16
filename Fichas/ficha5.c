f1e3e207e96974da8ebda989471f542d357d082b
guedes674 <guedes674@guedes>
2024-03-16 20:30:15 +0000

    ex 4 ficha 5

---
+++ Fichas/ficha5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+typedef struct aluno {
+    int numero;
+    char nome[100];
+    int miniT [6];
+    float teste;
+} Aluno;
+
+//1
+int nota (Aluno a){
+    int minitestes = 0, i = 0, total = 0;
+    float teste = a.teste;
+
+    while(a.miniT[i] < 6){
+        minitestes += a.miniT[i];
+        i++;
+    }
+    total = (minitestes*2)*0.2 + (teste)*0.8;
+
+    return total;
+}
+
+//2
+int procuraNum (int num, Aluno t[], int N){
+    int ret = -1, i;
+
+    for(i = 0; i < N; i++){
+        if(t[i].numero == num){
+            ret = i;
+        }
+    }
+    return ret;
+}
+
+//3
+void ordenaPorNum (Aluno t [], int N){
+    Aluno aux;
+    int i, j;
+
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            if(t[i].numero < t[j].numero){
+                aux = t[j];
+                t[j] = t[i];
+                t[i] = aux;
+            }
+        }
+    }
+}
